3ae4b774315f87938863dcd28fe2f641585ca33d
STELLA PONG KEXIN <stellapong0513@hotmail.com>
2017-11-12 06:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-let oldID = 1
 newID += 1000000000
 ageIsValid==currentAge
 randomInteger=Math.floor(randomNumber)
