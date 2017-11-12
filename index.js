c0841acf10af8503dd7ab8b37ccd14cc985f52a4
STELLA PONG KEXIN <stellapong0513@hotmail.com>
2017-11-12 06:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+newID = 1000000000
+newID = oldID++
+ageIsValid==currentAge
+randomInteger=floor(randomNumber)
+randomUserId=randomInteger
